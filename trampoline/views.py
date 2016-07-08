18b306e10ff718e7d0544fbf3b45d415aaf1b36a
Laurent Guilbert <laurent.guilbert91@gmail.com>
2016-07-08 14:02:09 +0200

    Add paginator and view mixin.

---
+++ trampoline/views.py
@@ -0,0 +1,37 @@
+"""
+Views for trampoline.
+"""
+from django.utils.functional import cached_property
+
+from trampoline.paginator import ESSearchPaginator
+
+
+class ESPaginationMixin(object):
+    page_size = 10
+
+    def get_search(self):
+        raise NotImplementedError
+
+    def get_page_number(self):
+        try:
+            number = int(self.request.GET.get('page', 1))
+        except (TypeError, ValueError):
+            pass
+        return number
+
+    def paginate_search(self):
+        search = self.get_search()
+        paginator = ESSearchPaginator(search, self.page_size)
+        page_number = self.get_page_number()
+        return paginator.page(page_number)
+
+    @cached_property
+    def page(self):
+        return self.paginate_search()
+
+    def get_context_data(self, *args, **kwargs):
+        context = (
+            super(ESPaginationMixin, self).get_context_data(*args, **kwargs)
+        )
+        context.update({'page': self.page})
+        return context
